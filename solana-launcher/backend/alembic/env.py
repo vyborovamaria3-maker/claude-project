3d7696de5c7b001eb308098d3987e8452f2117de
dima09090 <potapovdima354@gmail.com>
2026-08-08 08:21:22 +0300

    Include subscription settings in Alembic metadata

--- solana-launcher/backend/alembic/env.py
+++ solana-launcher/backend/alembic/env.py
@@ -12,6 +12,7 @@ from app.db.base import Base
 from app.models import analytics  # noqa: F401
 from app.models import auth_log  # noqa: F401
 from app.models import subscription_order  # noqa: F401
+from app.models import subscription_settings  # noqa: F401
 from app.models import user  # noqa: F401
 
 config = context.config
