ef748173a3439f76b73dd0c2a671a324d35b7d02
dima09090 <potapovdima354@gmail.com>
2026-08-10 08:07:59 +0300

    use testable intelligence view factory in admin entrypoint

--- admin-site/app/main_admin.py
+++ admin-site/app/main_admin.py
@@ -5,11 +5,8 @@ from fastapi import FastAPI
 from .analysis_api_v3 import build_analysis_router
 from .analysis_editor import LiveAnalysisProfileStore
 from .analysis_editor_api import build_analysis_editor_router
-from .intelligence_view import (
-    IntelligenceViewStore,
-    PostgresIntelligenceViewStore,
-    build_intelligence_router,
-)
+from .intelligence_view import build_intelligence_router
+from .intelligence_view_factory import build_intelligence_view_store
 from .main import create_app as create_base_app
 from .security_v2 import install_security
 from .services import TELEGRAM_TABLES
@@ -28,14 +25,7 @@ for _table in (
 def create_app() -> FastAPI:
     app = create_base_app()
     app.state.analysis_profiles = LiveAnalysisProfileStore(app.state.settings.audit_db_path)
-    if app.state.settings.intelligence_backend == "postgres":
-        app.state.intelligence_view = PostgresIntelligenceViewStore(
-            app.state.settings.intelligence_postgres_dsn
-        )
-    else:
-        app.state.intelligence_view = IntelligenceViewStore(
-            app.state.settings.intelligence_db_path
-        )
+    app.state.intelligence_view = build_intelligence_view_store(app.state.settings)
     app.include_router(build_analysis_router())
     app.include_router(build_analysis_editor_router())
     app.include_router(build_intelligence_router())
