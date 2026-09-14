466e91a72e30e6bd5ce0a88b4c039a2ea8806785
vyborovamaria3-maker <vyborovamaria3@gmail.com>
2026-09-15 00:41:40 +0300

    feat(kols): integrate directory flow feed and backtest in tab

--- solana-launcher/app/trade/kols-twitter/page.tsx
+++ solana-launcher/app/trade/kols-twitter/page.tsx
@@ -1,13 +1,15 @@
+import KOLBacktestPanel from "@/components/trade/KOLBacktestPanel";
 import KOLLiveTradeFeed from "@/components/trade/KOLLiveTradeFeed";
 import KOLTokenFlowPanel from "@/components/trade/KOLTokenFlowPanel";
 import KolsTwitterPanel from "@/components/trade/KolsTwitterPanel";
 
 export default function KolsTwitterPage() {
   return (
-    <>
+    <div className="min-w-0">
+      <KolsTwitterPanel />
       <KOLTokenFlowPanel />
       <KOLLiveTradeFeed />
-      <KolsTwitterPanel />
-    </>
+      <KOLBacktestPanel />
+    </div>
   );
 }
